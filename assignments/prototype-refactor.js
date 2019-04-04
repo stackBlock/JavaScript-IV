9347f6cf6e2b1cfc11e1169bcb0bb78a85572d2d
anthony <stackchain@gmail.com>
2019-04-04 16:48:25 -0400

    stretch goals done

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -196,55 +196,57 @@ class Hero extends Humanoid {
       language: 'Elvish',
     });
     
-  // console.log(mage.createdAt); // Today's date
-  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  // console.log(swordsman.healthPoints); // 15
-  // console.log(mage.name); // Bruce
-  // console.log(swordsman.team); // The Round Table
-  // console.log(mage.weapons); // Staff of Shamalama
-  // console.log(archer.language); // Elvish
-  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  // console.log(mage.takeDamage()); // Bruce took damage.
-  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
-  console.log(swordsman.cuts());
-  pause(4000);
-  console.log(archer.stabs());
-  pause(4000);
-  console.log(mage.slashes());
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
+//   console.log(swordsman.cuts());
+//   pause(4000);
+//   console.log(archer.stabs());
+//   pause(4000);
+//   console.log(mage.slashes());
   
   
